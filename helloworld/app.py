3fa17cb7f1fd506c12ad95ebb736bc7462b0f400
Vince Bloise <vbloise3@verizon.net>
2018-12-04 20:32:26 -0500

    Added introspect and PU, POST methods

--- helloworld/app.py
+++ helloworld/app.py
@@ -1,10 +1,15 @@
 from chalice import Chalice
+from chalice import BadRequestError, NotFoundError
 
 app = Chalice(app_name='helloworld')
+app.debug = True
 
 CITIES_TO_STATE = {
     'seattle': 'WA',
     'portland': 'OR',
+}
+
+OBJECTS = {
 }
 
 
@@ -13,27 +18,40 @@ def index():
     return {'hello': 'world', "name": "Vince"}
 
 
-@app.route('/cities/{city')
+@app.route('/cities/{city}')
 def state_of_city(city):
-    return {'state': CITIES_TO_STATE[city]}
+    try:
+        return {'state': CITIES_TO_STATE[city]}
+    except KeyError:
+        raise BadRequestError("Unknown city '%s', valid choices are: %s" % (
+            city, ', '.join(CITIES_TO_STATE.keys())))
 
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/resource/{value}', methods=['PUT'])
+def put_test(value):
+    return {"value": value}
+
+
+@app.route('/resource/{value}', methods=['POST'])
+def post_test(value):
+    return {"value": value}
+
+
+@app.route('/objects/{key}', methods=['GET', 'PUT'])
+def myobject(key):
+    request = app.current_request
+    if request.method == 'PUT':
+        OBJECTS[key] = request.json_body
+    elif request.method == 'GET':
+        try:
+            return {key: OBJECTS[key]}
+        except KeyError:
+            raise NotFoundError(key)
+        
+
+@app.route('/introspect')
+def introspect():
+    return app.current_request.to_dict()
+
+
+
